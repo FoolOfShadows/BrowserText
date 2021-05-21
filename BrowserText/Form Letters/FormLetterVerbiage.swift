5e910ff49959fed7c7aa8a98e2e65988861e6178
Fool Of Shadows <foolofshadows@gmail.com>
2021-05-21 12:38:03 -0500

    Filled in missing verbiage in the General Letter module and added another option.

--- BrowserText/Form Letters/FormLetterVerbiage.swift
+++ BrowserText/Form Letters/FormLetterVerbiage.swift
@@ -83,3 +83,21 @@ This letter is to inform you that due to a pattern of medical non-compliance I w
 let dsmslPayemntArrangement = """
 This letter is to inform you that as your account is seriously overdue and you have failed to keep the payment arrangement you made with my billing office, I will no longer be able to continue as your physician.
 """
+
+//let reinstatementDeclined = """
+//    While we appreciate your interest in re-establishing Whelchel Primary Care Medicine as your health care provider, we do not consider such an action to be prudent and therefore must decline.  If you have not yet found another health care provider we encourage you to do so as soon as possible.
+//    """
+
+//let reinstatementAccepted = """
+//    We appreciate your recent payment and are glad to be able to reinstate you as a patient of Whelchel Primary Care Medicine.
+//
+//    If in the future you find yourself unable to meet your financial obligations to us in a prompt manner, please contact our billing office at (903) 935-7101 extension 3 as soon as possible.  If it is determined at any point going forward that your account is more than 45 days past due and payment arrangements with our billing office have not been made or kept, we may withdraw our professional services with no further notice.  Note that any payment arrangements you may have made with our office prior to being dismissed are no longer in effect.
+//
+//    Because any existing appointments you had with our office have been cancelled, you will need to call us at (903) 935-7101 to schedule a new appointment.
+//
+//    We appreciate you working with us to resolve this issue and look forward to continuing to provide for your health care needs.
+//    """
+
+//let unableToAccept = """
+//    While we appreciate your interest in establishing Whelchel Primary Care Medicine as your health care provider, we will be unable to accept you as a patient of our practice at this time, and you will need to have your health care needs met by one of the other providers in the area.
+//    """
--- BrowserText/Form Letters/GeneralLetterVC.swift
+++ BrowserText/Form Letters/GeneralLetterVC.swift
@@ -48,12 +48,21 @@ class GeneralLetterVC: NSViewController {
     }
     @IBAction func reinstatementOK(_ sender: Any) {
         ltrBodyTextView.string = DefaultLetters.reinOK.rawValue
+        senderName.stringValue = DefaultLetters.rwSender.rawValue
+        senderTitle.stringValue = DefaultLetters.rwTitle.rawValue
     }
     @IBAction func reinstatmentDeclined(_ sender: Any) {
         ltrBodyTextView.string = DefaultLetters.reinNo.rawValue
+        senderName.stringValue = DefaultLetters.rwSender.rawValue
+        senderTitle.stringValue = DefaultLetters.rwTitle.rawValue
     }
     @IBAction func tagReminder(_ sender: Any) {
         ltrBodyTextView.string = DefaultLetters.tagRmndr.rawValue
+    }
+    @IBAction func unableToAccept(_ sender: Any) {
+        ltrBodyTextView.string = DefaultLetters.unableToAccept.rawValue
+        senderName.stringValue = DefaultLetters.rwSender.rawValue
+        senderTitle.stringValue = DefaultLetters.rwTitle.rawValue
     }
     @IBAction func russSenderTitle(_ sender: Any) {
         senderName.stringValue = DefaultLetters.rwSender.rawValue
@@ -74,13 +83,30 @@ class GeneralLetterVC: NSViewController {
     
     enum DefaultLetters:String {
         case reinOK = """
-        1
+        We appreciate your recent payment and are glad to be able to reinstate you as a patient of Whelchel Primary Care Medicine.
+            
+        If in the future you find yourself unable to meet your financial obligations to us in a prompt manner, please contact our billing office at (903) 935-7101 extension 3 as soon as possible.  If it is determined at any point going forward that your account is more than 45 days past due and payment arrangements with our billing office have not been made or kept, we may withdraw our professional services with no further notice.  Note that any payment arrangements you may have made with our office prior to being dismissed are no longer in effect.
+            
+        Because any existing appointments you had with our office have been cancelled, you will need to call us at (903) 935-7101 to schedule a new appointment.
+            
+        We appreciate you working with us to resolve this issue and look forward to continuing to provide for your health care needs.
         """
         case reinNo = """
-        2
+        While we appreciate your interest in re-establishing Whelchel Primary Care Medicine as your health care provider, we do not consider such an action to be prudent and therefore must decline.  If you have not yet found another health care provider we encourage you to do so as soon as possible.
+        
+        Thank you for your consideration,
         """
         case tagRmndr = """
-        3
+        According to our records it is time for a follow up appointment with Dr. Dawn Whelchel.
+        
+        The nature of some medications used in your treatment make it important to keep regularly scheduled appointments to insure effectiveness, reduce possible risks, and avoid compliance related prescription delays.
+        
+        Please call our office at (903) 935-7101 to schedule an appointment at your earliest convenience.
+        """
+        case unableToAccept = """
+        While we appreciate your interest in establishing Whelchel Primary Care Medicine as your health care provider, we will be unable to accept you as a patient of our practice at this time, and you will need to have your health care needs met by one of the other providers in the area.
+            
+        Thank you for your consideration,
         """
         case rwSender = "russ whelchel"
         case rwTitle = "Administrator"
